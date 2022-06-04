6518c9bc51082eb49d537cbbf7e9aec98ce85eef
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-06-04 11:33:20 -0400

    Move readDar to Dar class

--- src/main/java/org.jh.tools.daml/Dar.java
+++ src/main/java/org.jh.tools.daml/Dar.java
@@ -2,10 +2,19 @@ package org.jh.tools.daml;
 
 import com.daml.daml_lf_dev.DamlLf;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Dar
 {
+    private static final Logger LOGGER =  Logger.getLogger(Dar.class.getName());
+
     private String name;
     private Map<String,String> sources;
     private DamlLf.Archive damlLf;
@@ -15,6 +24,45 @@ public class Dar
         this.name = name;
         this.sources = sources;
         this.damlLf = damlLf;
+    }
+
+    public static Dar readDar(String filePath)
+    {
+        Path path = Paths.get(filePath);
+        DamlLf.Archive archiveProto = null;
+        Map<String,String> sources = new HashMap<>();
+
+        try(ZipInputStream is = new ZipInputStream(java.nio.file.Files.newInputStream(path)))
+        {
+
+            ZipEntry entry;
+            while((entry = is.getNextEntry()) != null)
+            {
+                String name = entry.getName();
+                LOGGER.info(name);
+
+                if (name.endsWith(".dalf"))
+                {
+                    byte[] bytes = is.readAllBytes();
+                    archiveProto = DamlLf.Archive.parseFrom(bytes);
+                    LOGGER.info(archiveProto.getHash());
+                }
+                else if (name.endsWith(".daml"))
+                {
+                    String file = new String(is.readAllBytes());
+                    sources.put(name, file);
+                }
+            }
+
+            return new Dar(path.getFileName().toString(), sources, archiveProto);
+        }
+        catch (IOException e)
+        {
+            //todo: clean-up
+            e.printStackTrace();
+            throw new RuntimeException("Failed to read archive:" + filePath);
+        }
+
     }
 
     public String getName()
--- src/main/java/org.jh.tools.daml/Upgrader.java
+++ src/main/java/org.jh.tools.daml/Upgrader.java
@@ -9,12 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class Upgrader
 {
@@ -26,8 +22,8 @@ public class Upgrader
         LOGGER.info("Current Archive Path=" + currentArchivePath);
         LOGGER.info("New Archive Path=" + newArchivePath);
 
-        DamlLf.Archive archiveOld = readDar(currentArchivePath).getDamlLf();
-        DamlLf.Archive archiveNew = readDar(newArchivePath).getDamlLf();
+        DamlLf.Archive archiveOld = Dar.readDar(currentArchivePath).getDamlLf();
+        DamlLf.Archive archiveNew = Dar.readDar(newArchivePath).getDamlLf();
 
         List<String> upgrades = identifyTemplatesToUpgrade(archiveOld, archiveNew);
         createAndWriteUpgradesToFiles(upgrades, outputPath);
@@ -83,42 +79,4 @@ public class Upgrader
         }
     }
 
-    private static Dar readDar(String filePath)
-    {
-        Path path = Paths.get(filePath);
-        DamlLf.Archive archiveProto = null;
-        Map<String,String> sources = new HashMap<>();
-
-        try(ZipInputStream is = new ZipInputStream(java.nio.file.Files.newInputStream(path)))
-        {
-
-            ZipEntry entry;
-            while((entry = is.getNextEntry()) != null)
-            {
-                String name = entry.getName();
-                LOGGER.info(name);
-
-                if (name.endsWith(".dalf"))
-                {
-                    byte[] bytes = is.readAllBytes();
-                    archiveProto = DamlLf.Archive.parseFrom(bytes);
-                    LOGGER.info(archiveProto.getHash());
-                }
-                else if (name.endsWith(".daml"))
-                {
-                    String file = new String(is.readAllBytes());
-                    sources.put(name, file);
-                }
-            }
-
-            return new Dar(path.getFileName().toString(), sources, archiveProto);
-        }
-        catch (IOException e)
-        {
-            //todo: clean-up
-            e.printStackTrace();
-            throw new RuntimeException("Failed to read archive:" + filePath);
-        }
-
-    }
 }
