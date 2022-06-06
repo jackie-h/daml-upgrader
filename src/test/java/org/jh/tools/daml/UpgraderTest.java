143460fc92c3417c27c083ca98dfe77e3aacc958
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-06-06 19:47:06 -0400

    Start adding end-to-end test

--- src/test/java/org/jh/tools/daml/UpgraderTest.java
+++ src/test/java/org/jh/tools/daml/UpgraderTest.java
@@ -3,10 +3,99 @@ package org.jh.tools.daml;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class UpgraderTest
 {
+
+    private void cleanBuildDar(String directory) throws IOException
+    {
+        File dir = Paths.get(directory).toFile();
+        execCmd("daml clean",  dir);
+        execCmd("daml build --log-level ERROR",  dir);
+    }
+
+    @Test
+    public void testEndToEnd() throws IOException
+    {
+        cleanBuildDar("daml-examples/init/parties");
+        cleanBuildDar("daml-examples/scenario1/v1");
+        cleanBuildDar("daml-examples/scenario1/v2");
+
+        Process sandbox = startSandbox();
+
+        execCmd("daml script --dar daml-examples/init/parties/.daml/dist/init-1.0.0.dar --script-name Parties:allocateParties --ledger-host localhost --ledger-port 6865");
+
+        String parties = execCmd("daml ledger list-parties --host localhost --port 6865");
+
+        Assert.assertTrue(parties.contains("\"bob\""));
+
+        execCmd("daml ledger upload-dar daml-examples/scenario1/v1/.daml/dist/carbon-1.0.0.dar --host localhost --port 6865");
+
+
+        //String res2 = execCmd("daml test --files daml-examples/init/parties/daml/Parties.daml");
+        //String res = execCmd("daml test --files src/test/resources/Scenario1Test.daml");
+
+        sandbox.destroyForcibly();
+    }
+
+    public Process startSandbox() throws IOException
+    {
+        Process proc = Runtime.getRuntime().exec("daml sandbox");
+
+        try
+        {
+            TimeUnit.MILLISECONDS.sleep(10);
+        }
+        catch (InterruptedException e)
+        {
+            //ok
+        }
+
+        return proc;
+    }
+
+    public static String execCmd(String cmd) throws IOException
+    {
+        return execCmd(cmd, null);
+    }
+
+    public static String execCmd(String cmd, File dir) throws IOException
+    {
+        String result = null;
+        Process proc = (dir == null) ? Runtime.getRuntime().exec(cmd): Runtime.getRuntime().exec(cmd, new String[]{}, dir);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+        // Read the output from the command
+        String s = null;
+        StringBuilder out = new StringBuilder();
+        while ((s = stdInput.readLine()) != null) {
+            out.append(s).append("\n");
+        }
+
+        // Read any errors from the attempted command
+        StringBuilder err = new StringBuilder();
+        String e = null;
+        while ((e = stdError.readLine()) != null) {
+            err.append(e).append("\n");
+        }
+
+        Assert.assertEquals("", err.toString());
+
+        return out.toString();
+    }
+
     @Test
     public void testScenario1()
     {
