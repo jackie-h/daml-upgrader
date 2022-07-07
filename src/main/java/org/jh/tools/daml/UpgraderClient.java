daea4825b19104d23231176486245a5285d20540
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-07-07 13:15:11 -0400

    Add usage message

--- src/main/java/org/jh/tools/daml/UpgraderClient.java
+++ src/main/java/org/jh/tools/daml/UpgraderClient.java
@@ -4,16 +4,25 @@ public class UpgraderClient
 {
     public static void main(String[] args)
     {
-        String filePath1 = args[0];
-        String filePath2 = args[1];
-        String outputDirectory = args[2];
-        String dataDependencies = null;
-
-        if(args.length == 4)
+        System.out.println("Welcome to the DAML smart contract upgrade contract generator.\n");
+        System.out.println("Given two DAR files, the upgrader will generate upgrade contracts for cases where upgrades can be automated\n");
+        if (args.length < 3 || args.length > 4)
         {
-            dataDependencies = args[4];
+            System.out.println("Usage: <path_to_dar_upgrade_from> <path_to_dar_to_upgrade_to> <output_directory> [<data_dependencies>]");
         }
+        else
+        {
+            String filePath1 = args[0];
+            String filePath2 = args[1];
+            String outputDirectory = args[2];
+            String dataDependencies = null;
 
-        Upgrader.createUpgrades(filePath1, filePath2, outputDirectory, dataDependencies);
+            if (args.length == 4)
+            {
+                dataDependencies = args[4];
+            }
+
+            Upgrader.createUpgrades(filePath1, filePath2, outputDirectory, dataDependencies);
+        }
     }
 }
