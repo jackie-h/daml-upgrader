1d636c25230036b65dafde96a639bae3426e9033
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-07-24 14:39:03 -0400

    Move data dep example into different module

--- src/test/java/org/jh/tools/daml/UpgraderTest.java
+++ src/test/java/org/jh/tools/daml/UpgraderTest.java
@@ -28,30 +28,30 @@ public class UpgraderTest
                 "daml-examples/scenario1/v2/.daml/dist/carbon-2.0.0.dar",
                 "target/scenario1", "../../daml-examples/data/v1/.daml/dist/finance-1.0.0.dar");
 
-        Assert.assertEquals("---------------------------------------------------------------------------------------------------------------\n" +
-                        "| Module               | Template              | Result                                                       |\n" +
-                        "---------------------------------------------------------------------------------------------------------------\n" +
-                        "| Intro.SchemaWithData | ContractWithType      | Template has a type that is currently not supported          |\n" +
-                        "| Intro.SchemaWithData | ContractWithDataDep   | Ok!                                                          |\n" +
-                        "| Intro.SchemaWithData | ContractWithDataList  | Template has a type that is currently not supported          |\n" +
-                        "| Intro.SchemaWithData | ContractWithData      | Ok!                                                          |\n" +
-                        "| Intro.MultiParty     | Agreement             | Don't know how to upgrade contracts with >2 parties yet      |\n" +
-                        "| Intro.MultiParty     | Pending               | Don't know how to upgrade contracts with >2 parties yet      |\n" +
-                        "| Intro.SchemaChanges  | FieldBecomesMandatory | Template schema changed in a way that is not auto-upgradable |\n" +
-                        "| Intro.SchemaChanges  | Same                  | Ok!                                                          |\n" +
-                        "| Intro.SchemaChanges  | AddField              | Template schema changed in a way that is not auto-upgradable |\n" +
-                        "| Intro.SchemaChanges  | FieldTypeChange       | Template schema changed in a way that is not auto-upgradable |\n" +
-                        "| Intro.SchemaChanges  | AddOptionalField      | Ok!                                                          |\n" +
-                        "| Intro.SchemaChanges  | RemoveField           | Ok!                                                          |\n" +
-                        "| Intro.SchemaChanges  | FieldBecomesOptional  | Template schema changed in a way that is not auto-upgradable |\n" +
-                        "| Intro.SchemaChanges  | SameWithSetPrimitives | Ok!                                                          |\n" +
-                        "| Intro.SchemaChanges  | FieldNameChange       | Template schema changed in a way that is not auto-upgradable |\n" +
-                        "| Intro.SchemaChanges  | ReorderField          | Ok!                                                          |\n" +
-                        "| Carbon               | CarbonCertProposal    | Ok!                                                          |\n" +
-                        "| Carbon               | CarbonCert            | Ok!                                                          |\n" +
-                        "| Intro.Invite         | Invitation            | Ok!                                                          |\n" +
-                        "| Intro.Iou            | Iou                   | Ok!                                                          |\n" +
-                        "---------------------------------------------------------------------------------------------------------------\n",
+        Assert.assertEquals("-----------------------------------------------------------------------------------------------------------------------\n" +
+                        "| Module                       | Template              | Result                                                       |\n" +
+                        "-----------------------------------------------------------------------------------------------------------------------\n" +
+                        "| Intro.SchemaWithData         | ContractWithType      | Template has a type that is currently not supported          |\n" +
+                        "| Intro.SchemaWithData         | ContractWithDataList  | Template has a type that is currently not supported          |\n" +
+                        "| Intro.SchemaWithData         | ContractWithData      | Ok!                                                          |\n" +
+                        "| Intro.MultiParty             | Agreement             | Don't know how to upgrade contracts with >2 parties yet      |\n" +
+                        "| Intro.MultiParty             | Pending               | Don't know how to upgrade contracts with >2 parties yet      |\n" +
+                        "| Intro.SchemaChanges          | FieldBecomesMandatory | Template schema changed in a way that is not auto-upgradable |\n" +
+                        "| Intro.SchemaChanges          | Same                  | Ok!                                                          |\n" +
+                        "| Intro.SchemaChanges          | AddField              | Template schema changed in a way that is not auto-upgradable |\n" +
+                        "| Intro.SchemaChanges          | FieldTypeChange       | Template schema changed in a way that is not auto-upgradable |\n" +
+                        "| Intro.SchemaChanges          | AddOptionalField      | Ok!                                                          |\n" +
+                        "| Intro.SchemaChanges          | RemoveField           | Ok!                                                          |\n" +
+                        "| Intro.SchemaChanges          | FieldBecomesOptional  | Template schema changed in a way that is not auto-upgradable |\n" +
+                        "| Intro.SchemaChanges          | SameWithSetPrimitives | Ok!                                                          |\n" +
+                        "| Intro.SchemaChanges          | FieldNameChange       | Template schema changed in a way that is not auto-upgradable |\n" +
+                        "| Intro.SchemaChanges          | ReorderField          | Ok!                                                          |\n" +
+                        "| Intro.SchemaWithDataExternal | ContractWithDataDep   | Ok!                                                          |\n" +
+                        "| Carbon                       | CarbonCertProposal    | Ok!                                                          |\n" +
+                        "| Carbon                       | CarbonCert            | Ok!                                                          |\n" +
+                        "| Intro.Invite                 | Invitation            | Ok!                                                          |\n" +
+                        "| Intro.Iou                    | Iou                   | Ok!                                                          |\n" +
+                        "-----------------------------------------------------------------------------------------------------------------------\n",
                 archiveDiffs.report());
 
         DamlCommand.cleanBuildDar("target/scenario1");
@@ -78,10 +78,10 @@ public class UpgraderTest
                         "Intro.SchemaChanges.UpgradeSameWithSetPrimitives[UpgradeSameWithSetPrimitivesProposal]\n" +
                         "Intro.SchemaChanges.UpgradeSame[UpgradeSameAgreement]\n" +
                         "Intro.SchemaChanges.UpgradeSame[UpgradeSameProposal]\n" +
-                        "Intro.SchemaWithData.UpgradeContractWithDataDep[UpgradeContractWithDataDepAgreement]\n" +
-                        "Intro.SchemaWithData.UpgradeContractWithDataDep[UpgradeContractWithDataDepProposal]\n" +
                         "Intro.SchemaWithData.UpgradeContractWithData[UpgradeContractWithDataAgreement]\n" +
-                        "Intro.SchemaWithData.UpgradeContractWithData[UpgradeContractWithDataProposal]",
+                        "Intro.SchemaWithData.UpgradeContractWithData[UpgradeContractWithDataProposal]\n" +
+                        "Intro.SchemaWithDataExternal.UpgradeContractWithDataDep[UpgradeContractWithDataDepAgreement]\n" +
+                        "Intro.SchemaWithDataExternal.UpgradeContractWithDataDep[UpgradeContractWithDataDepProposal]",
                 String.join("\n", templates));
     }
 
